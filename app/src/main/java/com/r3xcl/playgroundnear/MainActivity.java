fb7889cc79ae269ef2d269d76195db94a8f3481f
r3xcl <pervih.matvey@gmail.com>
2021-06-08 15:13:51 +0300

    Add bottom navigation with nav. component

--- app/src/main/java/com/r3xcl/playgroundnear/MainActivity.java
+++ app/src/main/java/com/r3xcl/playgroundnear/MainActivity.java
@@ -1,10 +1,20 @@
 package com.r3xcl.playgroundnear;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
